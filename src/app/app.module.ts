83045729e56378b501e70aefdc7607d27f50d4c6
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-02-04 20:59:44 +0100

    Add material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { WeatherComponent } from "./components/weather/weather.component";
@@ -7,12 +7,14 @@ import { RoundPipe } from "./pipes/round.pipe";
 import { MemoComponent } from "./components/components/memo/memo.component";
 import { ExchangeRateComponent } from "./components/components/exchange-rate/exchange-rate.component";
 import { FormsModule } from "@angular/forms";
-
 import { registerLocaleData } from "@angular/common";
-import localeFr from "@angular/common/locales/fr";
-import localeFrExtra from "@angular/common/locales/extra/se";
+import localeSe from "@angular/common/locales/se";
+import { DateTimeComponent } from "./components/date-time/date-time.component";
+import { AsideNavComponent } from "./components/aside-nav/aside-nav.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularMaterialModule } from "./angular-material.module";
 
-registerLocaleData(localeFr, "se-SE", localeFrExtra);
+registerLocaleData(localeSe, "se-SE");
 
 @NgModule({
   declarations: [
@@ -20,10 +22,19 @@ registerLocaleData(localeFr, "se-SE", localeFrExtra);
     WeatherComponent,
     RoundPipe,
     MemoComponent,
-    ExchangeRateComponent
+    ExchangeRateComponent,
+    DateTimeComponent,
+    AsideNavComponent
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NoopAnimationsModule,
+    AngularMaterialModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
